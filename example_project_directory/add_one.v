ed24754eae1972c47f7615f8ae788ef6c236aed8
ben <feasterben@hotmail.com>
2023-09-15 16:34:22 -0400

    svg

--- example_project_directory/add_one.v
+++ example_project_directory/add_one.v
@@ -1,9 +1,9 @@
 `timescale 1ns / 1ps
 
 module add_one(
-	input [15:0] sel0,
-	input [15:0] sel1,
-	output reg [15:0] out);
+	input [7:0] sel0,
+	input [7:0] sel1,
+	output reg [7:0] out);
 	
 	always @(*)
 		out = sel0 + sel1;
