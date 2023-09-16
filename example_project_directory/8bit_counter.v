b1e5aad506230dd0e8f550f2746c52ce302dd338
ben <55043306+benipoo@users.noreply.github.com>
2023-09-15 22:06:26 -0400

    Update 8bit_counter.v

--- example_project_directory/8bit_counter.v
+++ example_project_directory/8bit_counter.v
@@ -1,14 +1,16 @@
 
-module 8bit_counter(count,enable,clk,rst_n);
-
-    input enable,clk,rst_n;  
-    output reg[3:0] count;  
+module 8bit_counter(
+    input enable,
+    input clk,
+    input rst_n,  
+    output reg[7:0] count);  
     
     always @(posedge clk or negedge rst_n)  
         begin  
-            if(~rst_n) counter <= 4'b0000;  
-        else if(enable)  
-            counter <= counter + 4'b0001;  
+            if(~rst_n) 
+                counter <= 4'b0000;  
+            else if(enable)  
+                counter <= counter + 4'b0001;  
         end
         
  endmodule  
